bfed66cd47b951578b03fa971fc7ec731dfcbdbb
bapk04 <ngogiakiet20012004@gmail.com>
2025-11-24 13:00:13 +0700

    re database

--- admin/Database.php
+++ admin/Database.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $servername='localhost';
 $username='root';
@@ -8,15 +7,4 @@ $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql: ' . mysqli_connect_error());
 }
-=======
-<?php
-$servername='localhost';
-$username='root';
-$password='';
-$dbname = "moviebook";
-$conn=mysqli_connect($servername,$username,$password,"$dbname");
-if(!$conn){
-   die('Could not Connect My Sql:' .mysqli_error());
-}
->>>>>>> ad940724cdf419c4c15e5ee851db482cf75fb51e
 ?>
--- database_connection.php
+++ database_connection.php
@@ -2,10 +2,6 @@
 
 //database_connection.php
 
-<<<<<<< HEAD
 $connect = new PDO("mysql:host=localhost;dbname=cinelux", "root", "");
-=======
-$connect = new PDO("mysql:host=localhost;dbname=moviebook", "root", "");
->>>>>>> ad940724cdf419c4c15e5ee851db482cf75fb51e
 
 ?>
