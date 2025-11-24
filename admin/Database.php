70eadd60f6e271bfe0226e5df1120e092916054a
bapk04 <ngogiakiet20012004@gmail.com>
2025-11-24 12:57:55 +0700

    rebase code

--- admin/Database.php
+++ admin/Database.php
@@ -3,7 +3,7 @@ $servername='localhost';
 $username='root';
 $password='';
 $dbname = "cinelux";
-$conn=mysqli_connect($servername,$username,$password,$dbname);
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql: ' . mysqli_connect_error());
 }
